e90fe3706a8cdea2049558be62cd47244bf147b4
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-04-01 18:24:02 -0400

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -25,22 +25,22 @@ const Icons = require('../UI/Icons');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('support')
-        .setDescription('Support server of this Bot'),
+        .setDescription('Server of EchoBot Modmail'),
     async execute(interaction) {
-        const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-        const githubLink = "https://github.com/GlaceYT";
-        const replitLink = "https://replit.com/@GlaceYT";
-        const youtubeLink = "https://www.youtube.com/@GlaceYT";
+        const supportServerLink = "https://discord.gg/neEV7uKDja";
+        const githubLink = "Blank";
+        const replitLink = "Blank";
+        const youtubeLink = "Blank";
 
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
-                name: 'Support Server',
+                name: 'EchoBot Support',
                 iconURL: Icons.dotIcon,
                 url: 'https://discord.gg/xQF9f9yUEM'
             })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1264134884432285766/1293631187730038885/GlaceYT.gif?ex=6708136d&is=6706c1ed&hm=45c799968ea8269c7d9e77fc4c1dd41f793cd56808d206a496354bf84be434fb&')
+            .setImage('https://cdn.discordapp.com/attachments/1356367098267566240/1356755852689805473/standard_3.gif?ex=67edb8da&is=67ec675a&hm=0e6c922449aea599ddc98ce67c94b5506a30d77a6aa8088231b7eae406e10186&')
             .setTimestamp();
 
         await interaction.reply({ embeds: [embed] });
